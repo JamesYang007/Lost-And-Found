7119cd39b32244fd60bdbc4ec95ec37d081617fc
James Yang <jamesyang916@gmail.com>
2020-01-25 22:53:45 -0500

    Database process change

---
+++ laf/dbprocess/database.py
@@ -0,0 +1,4 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
--- laf/dbprocess/dbprocess.py
+++ laf/dbprocess/dbprocess.py
@@ -1,41 +1,65 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
 # create a global variable of the object Laf inside this file.
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/posts.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-lafobj = laf()
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    imagepath = db.Column(db.String(100))
+    #latitude = db.Column(db.Double)
+    #longitude = db.Column(db.Double)
+    #uni = db.Column(db.String(20))
+    #phone = db.Column(db.String(15)) # should it be String or Integer?
+    #keywords = db.Column(db.String(100))
+    desc = db.Column(db.String(1000))
+    #lost = db.Column(db.Boolean)
 
 # function goes here. I take in an Object and set the model fields based on the attributes of the given object
 
-def add_post(imagepath, latitude, longitude, uni, phone, keywords, desc, lost):
-    lafobj = laf(laf.imagepath = imagepath, laf.latitude = latitude, laf.longitude = longitude, laf.uni = uni, laf.phone = phone, laf.keywords = keywords, laf.desc = desc, laf.lost = lost)
-    db.session.add(lafobj)
-
-    return redirect(url_for('index')) # stay on the home page
-
-# Route to index (default) page with blank database.
 @app.route('/')
 def index():
-    lost = Todo.query.filter_by(lost = True).all()
-    found = Todo.query.filter_by(lost = False).all()
+    return redirect(url_for('index'))
 
-    return render_template('index.html',
-       incomplete = incomplete, complete = complete)
+def add_post(imagepath,
+             #latitude,
+             #longitude,
+             #uni,
+             #phone,
+             #keywords,
+             desc
+             #lost
+             ):
 
-# Route used to add items to the database.
-    db.session.add(todo)
+    params = {
+        "imagepath" : imagepath,
+        "desc" : desc
+    }
+
+    entry = Entry(**params)
+    db.session.add(entry)
     db.session.commit()
+    #return redirect(url_for('index')) # stay on the home page
 
-    return redirect(url_for('index')) # stay on the home page
+## Route to index (default) page with blank database.
+#@app.route('/')
+#def index():
+#    lost = Todo.query.filter_by(lost = True).all()
+#    found = Todo.query.filter_by(lost = False).all()
+#
+#    return render_template('index.html',
+#       incomplete = incomplete, complete = complete)
+#
+## Route used to add items to the database.
+#    db.session.add(todo)
+#    db.session.commit()
+#
+#    return redirect(url_for('index')) # stay on the home page
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
+    add_post('./hello', 'some description')
