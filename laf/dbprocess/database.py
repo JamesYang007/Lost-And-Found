ee256e028381361ac4842ac6b437b0ff071021f8
James Yang <jamesyang916@gmail.com>
2020-01-26 07:11:31 -0500

    Wow that was a lot of shit in 1 commit

--- laf/dbprocess/database.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_migrate import Migrate
-from flask_sqlalchemy import SQLAlchemy
-
---
+++ laf/imgprocess.py
@@ -0,0 +1,32 @@
+import os
+import hashlib
+import threading
+
+# Prefix directory to append to each files
+IMAGE_PREFIX_DIR = "./images"
+
+# Save file using given filename
+def process_image(fileobj):
+    filepath = os.path.join(IMAGE_PREFIX_DIR, "__tmp" + threading.current_thread().ident + "__")
+    fileobj.save(filepath)
+    unique_filepath = ''.join([hash_file(filepath), '.jpg'])
+    os.rename(filepath, unique_filepath)
+    return unique_filepath
+
+# MD5 hash file content to name file
+def hash_file(filepath):
+    BUF_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+    md5 = hashlib.md5()
+
+    with open(filepath, 'rb') as f:
+        while True:
+            data = f.read(BUF_SIZE)
+            if not data:
+                break
+            md5.update(data)
+
+    return md5.hexdigest()
+
+if __name__ == "__main__":
+    print(hash_file(os.path.join(IMAGE_PREFIX_DIR, 'IMG_0626.jpeg')))
